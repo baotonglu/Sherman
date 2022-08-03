49aa753be976e2cac85d1802d63961fd3cb55f07
baotonglu <lubaotong22@gmail.com>
2022-08-03 16:15:15 +0900

    remote random key generation

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -108,8 +108,8 @@ void thread_run(int id) {
   uint64_t end_warm_key = kWarmRatio * kKeySpace;
   for (uint64_t i = 1; i < end_warm_key; ++i) {
     if (i % all_thread == my_id) {
-      auto flag = tree->insert(to_key(i), i * 2);
-      //auto flag = tree->insert(i, i * 2);
+      //auto flag = tree->insert(to_key(i), i * 2);
+      auto flag = tree->insert(i, i * 2);
       if(flag == 2) total_items.fetch_add(1);
       if(flag == 0) fail_items.fetch_add(1);
     }
